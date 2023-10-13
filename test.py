497e2d5c9ed3747da2410760a1711e77e7d0bf0a
Quazi Marufur Rahman <maruf@amagroup.io>
2023-10-13 07:01:39 +0000

    don't need this plot

--- test.py
+++ test.py
@@ -153,16 +153,16 @@ def test(data,
                         f.write(('%g ' * len(line)).rstrip() % line + '\n')
 
             # W&B logging - Media Panel Plots
-            if len(wandb_images) < log_imgs and wandb_logger.current_epoch > 0:  # Check for test operation
-                if wandb_logger.current_epoch % wandb_logger.bbox_interval == 0:
-                    box_data = [{"position": {"minX": xyxy[0], "minY": xyxy[1], "maxX": xyxy[2], "maxY": xyxy[3]},
-                                 "class_id": int(cls),
-                                 "box_caption": "%s %.3f" % (names[cls], conf),
-                                 "scores": {"class_score": conf},
-                                 "domain": "pixel"} for *xyxy, conf, cls in pred.tolist()]
-                    boxes = {"predictions": {"box_data": box_data, "class_labels": names}}  # inference-space
-                    wandb_images.append(wandb_logger.wandb.Image(img[si], boxes=boxes, caption=path.name))
-            wandb_logger.log_training_progress(predn, path, names) if wandb_logger and wandb_logger.wandb_run else None
+            # if len(wandb_images) < log_imgs and wandb_logger.current_epoch > 0:  # Check for test operation
+            #     if wandb_logger.current_epoch % wandb_logger.bbox_interval == 0:
+            #         box_data = [{"position": {"minX": xyxy[0], "minY": xyxy[1], "maxX": xyxy[2], "maxY": xyxy[3]},
+            #                      "class_id": int(cls),
+            #                      "box_caption": "%s %.3f" % (names[cls], conf),
+            #                      "scores": {"class_score": conf},
+            #                      "domain": "pixel"} for *xyxy, conf, cls in pred.tolist()]
+            #         boxes = {"predictions": {"box_data": box_data, "class_labels": names}}  # inference-space
+            #         wandb_images.append(wandb_logger.wandb.Image(img[si], boxes=boxes, caption=path.name))
+            # wandb_logger.log_training_progress(predn, path, names) if wandb_logger and wandb_logger.wandb_run else None
 
             # Append to pycocotools JSON dictionary
             if save_json:
@@ -187,6 +187,7 @@ def test(data,
                 scale_coords(img[si].shape[1:], tbox, shapes[si][0], shapes[si][1])  # native-space labels
                 if plots:
                     confusion_matrix.process_batch(predn, torch.cat((labels[:, 0:1], tbox), 1))
+                    
 
                 # Per target class
                 for cls in torch.unique(tcls_tensor):
